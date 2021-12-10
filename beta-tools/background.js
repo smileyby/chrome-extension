294284ebe7c63692b4f544e46a41e7a19182f37f
yuboyang <smileyby@126.com>
2021-12-10 14:12:22 +0800

    feat(update): v

--- beta-tools/background.js
+++ beta-tools/background.js
@@ -16,6 +16,7 @@ chrome.tabs.onCreated.addListener(function(){
     // 必须在 manifest -> permissions 中添加 tabs 权限
     let url = tabs[0].url;
     let pendingUrl = tabs[0].pendingUrl;
+    console.log('标记isEnabled：', isEnabled);
     if (url === '' && pendingUrl == "chrome://newtab/" && !isEnabled) {
       chrome.tabs.update(tabs[0].id, {url: 'chrome-search://local-ntp/local-ntp.html'});
     }
